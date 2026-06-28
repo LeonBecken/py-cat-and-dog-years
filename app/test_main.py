35322b5950f78c25a6b195f3352c6895ebcd5d3c
Dmytro <dmitryibezkrovnyi@gmail.com>
2026-06-29 00:26:58 +0200

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,8 +1,7 @@
 from app.main import get_human_age
-import pytest
     
 
-def test_should_return_list_of_integers():
+def test_should_return_two_elements():
     result = get_human_age(10, 10)
     assert len(result) == 2
 
@@ -20,6 +19,10 @@ def test_should_return_one_when_age_fifteen():
     assert get_human_age(15, 15) == [1, 1]
 
 
+def test_should_return_one_when_age_is_twenty_three():
+    assert get_human_age(23, 23) == [1, 1]
+
+
 def test_should_return_two_year_when_age_twentyfour():
     assert get_human_age(24, 24) == [2, 2]
 
@@ -34,3 +37,7 @@ def test_should_return_three_cats_and_two_dogs():
 
 def test_should_return_three_when_age_more_twentynine():
     assert get_human_age(29, 29) == [3, 3]
+
+
+def test_should_return_correct_human_age_for_large_numbers():
+    assert get_human_age(100, 100) == [21, 17]
